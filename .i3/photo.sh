cdd2cbc98f2158085f97271310398c5ee5854c61
Daniel Ferreira de Lima <daniel.ferreira@adtsys.com.br>
2016-07-06 22:38:09 -0300

    New i3 stuff

--- .i3/photo.sh
+++ .i3/photo.sh
@@ -1,4 +1,4 @@
-FILE=~/Pictures/Screenshot-$USER-$(date +%Y%M%d-%H%M%S).png  
+FILE=~/Pictures/Screenshot-$USER-$(date +%Y%m%d-%H%M%S).png  
 import $FILE
 i3-nagbar -m "$FILE created"  &
 sleep 4 && killall i3-nagbar
